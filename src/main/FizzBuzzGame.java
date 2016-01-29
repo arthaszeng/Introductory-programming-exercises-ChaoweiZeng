a53bc302b854c701f4dea9b4b31b6bd0c80e7e1c
slizeng <251231296@qq.com>
2016-01-30 01:27:50 +0800

    Add method buzz(), and add test testBuzzIsRight(), done

--- src/main/FizzBuzzGame.java
+++ src/main/FizzBuzzGame.java
@@ -8,11 +8,20 @@ public class FizzBuzzGame {
     public void fizz(int max) {
         for (int i = 1; i <= max; i++) {
             if (i % 3 == 0) {
-                System.out.println("fizz");
+                System.out.println("Fizz");
             }else {
                 System.out.println(i);
             }
         }
     }
 
+    public void buzz(int max) {
+        for (int i = 1; i <= max; i++) {
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
 }
--- src/test/FizzBuzzGameTest.java
+++ src/test/FizzBuzzGameTest.java
@@ -28,6 +28,14 @@ public class FizzBuzzGameTest {
         FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
         fizzBuzzGame.fizz(15);
 
-        assertEquals("1\n2\nfizz\n4\n5\nfizz\n7\n8\nfizz\n10\n11\nfizz\n13\n14\nfizz\n", outContent.toString());
+        assertEquals("1\n2\nFizz\n4\n5\nFizz\n7\n8\nFizz\n10\n11\nFizz\n13\n14\nFizz\n", outContent.toString());
+    }
+
+    @Test
+    public void testBuzzIsRight() throws Exception {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        fizzBuzzGame.buzz(15);
+
+        assertEquals("1\n2\n3\n4\nBuzz\n6\n7\n8\n9\nBuzz\n11\n12\n13\n14\nBuzz\n", outContent.toString());
     }
 }
