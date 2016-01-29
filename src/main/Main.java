27387d8355f6ef87a0288d7d9b31897dc59f8eff
slizeng <251231296@qq.com>
2016-01-29 23:37:23 +0800

    first one done

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("*");
+    }
+
+    public void oneAsterisk() {
+        System.out.println("*");
     }
 }
--- src/test/exampleTest.java
+++ src/test/exampleTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class exampleTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testOutputOneAsterisk() throws Exception {
+        Main main = new Main();
+        main.oneAsterisk();
+
+        assertEquals("*\n", outContent.toString());
+    }
 }
