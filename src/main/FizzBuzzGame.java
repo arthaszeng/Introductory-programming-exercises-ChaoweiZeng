c2e385a282908fe8aaafa516a92611744ac33a84
slizeng <251231296@qq.com>
2016-01-30 03:57:51 +0800

    refactor finally, no warning ,and codes more clear.

--- src/main/FizzBuzzGame.java
+++ src/main/FizzBuzzGame.java
@@ -7,27 +7,19 @@ public class FizzBuzzGame {
 
     public void fizz(int max) {
         for (int i = 1; i <= max; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }else {
-                System.out.println(i);
-            }
+            System.out.println((i % 3) == 0 ? "Fizz" : i);
         }
     }
 
     public void buzz(int max) {
         for (int i = 1; i <= max; i++) {
-            if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }else {
-                System.out.println(i);
-            }
+            System.out.println((i % 5) == 0 ? "Buzz" : i);
         }
     }
 
     public void fizzBuzz(int max) {
         for (int i = 1; i <= max; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
                 System.out.println("FizzBuzz");
             }else if(i % 3 == 0){
                 System.out.println("Fizz");
